801d8238676da21b8a03f4816a7dd07391277172
susilnem <susilsaurav@gmail.com>
2022-08-03 16:52:19 +0545

    Project setup for Django rest framwork

---
+++ config/urls.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Booking API",
+        default_version='v1',
+        description="Api Documentation of Booking",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/v1/', include([
+        path('swagger/', schema_view.with_ui('swagger',
+             cache_timeout=0), name='schema-swagger-ui'),
+        path('redoc/', schema_view.with_ui('redoc',
+             cache_timeout=0), name='schema-redoc'),
+    ])),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
